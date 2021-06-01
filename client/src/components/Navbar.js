5ac07dc2ed2924df70d01c68cf1ac8555a4066d7
Shiqi He <she010@uottawa.ca>
2021-06-01 10:32:57 -0400

    tab icon and nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,11 +7,6 @@ import {
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
     Container,
 } from "reactstrap";
 
@@ -24,33 +19,16 @@ const NavbarComponent = (props) => {
         <div>
             <Navbar color="dark" dark expand="md">
                 <Container>
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarBrand href="/">Shopping List</NavbarBrand>
                     <NavbarToggler onClick={toggle} />
                     <Collapse isOpen={isOpen} navbar>
                         <Nav className="mr-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">
-                                    Components
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">
+                                <NavLink href="https://github.com/ShiqiHe000/MERN-Stack-Shopping-List">
                                     GitHub
                                 </NavLink>
                             </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>Option 1</DropdownItem>
-                                    <DropdownItem>Option 2</DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>Reset</DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
                         </Nav>
-                        <NavbarText>Simple Text</NavbarText>
                     </Collapse>
                 </Container>
             </Navbar>
